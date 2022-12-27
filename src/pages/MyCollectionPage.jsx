d1b706a6bc4ec9e11d1e83d94a2e95aa879c15db
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-27 17:39:08 -0300

    create new page to display user collection

---
+++ src/pages/MyCollectionPage.jsx
@@ -0,0 +1,11 @@
+import Button from "../components/Button";
+
+function MyCollection() {
+  return (
+    <div className="w-20">
+      <Button primary>Hello</Button>
+    </div>
+  );
+}
+
+export default MyCollection;
