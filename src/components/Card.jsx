a2dc035416509ab42a95ff54371fee45350fe058
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-28 15:03:37 -0300

    implement modal styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,21 @@ import Button from "./Button";
 
 function Card(props) {
   return (
-    <div className="relative px-3 pb-3 py-1 bg-purple-600 lg:w-100  rounded-lg hover:shadow-lg hover:scale-105 space-y-0 duration-100">
+    <div className="relative px-3 pb-3 py-0 bg-purple-600 lg:w-100  rounded-lg hover:shadow-lg hover:scale-105 space-y-3 duration-100">
       <div className="shadow-lg rotate-12 py-8 px-3 rounded-full text-sm bg-green-500 absolute -right-5 -top-3 z-10 text-white">
         undreamt
       </div>
-      <h3 className="lg:text-md text-white font-semibold ">
-        {props.card.unity}
-      </h3>
+      <div className="h-4">
+        <h3 className="lg:text-md text-white font-semibold ">
+          {props.card.unity}
+        </h3>
+      </div>
       <div className="border-4 w-full overflow-hidden border-white relative flex justify-center rounded shadow-inner">
-        <div className="absolute -bottom-1 text-center bg-white h-6 border-4 border-b-none w-14 rounded border-white">
+        <div className="absolute -bottom-1 text-center bg-white h-7 border-b-purple-500 border-4 border-b-none w-20 rounded border-white">
           <div
-            className={`bg-green-400 h-6 w-${props.card.xp}/6 justify-center text-xs sm:text-md`}
+            className={`bg-purple-600 h-10 w-${props.card.xp}/6 justify-center text-xs sm:text-md`}
           >
-            <div className="flex justify-center w-12">
+            <div className="text-white text-lg leading-3 absolute top-1 left-3 font-bold">
               level:{props.card.level}
             </div>
           </div>
--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,15 +1,39 @@
+import Button from "./Button";
+
 function CardModal(props) {
   const closeModal = props.closeModal;
   return (
     <div>
-      <div
-        onClick={closeModal}
-        className="fixed z-50 w-full h-full bg-black/50 p-4"
-      ></div>
-      <div>
-        <h1 className="fixed z-50 inset-20 h-3/4 p-10 bg-white">
-          {props.cardData.unity}
-        </h1>
+      <div className="flex justify-center items-center h-screen">
+        <div
+          onClick={closeModal}
+          className="fixed z-50 w-full h-full bg-black/50 p-4"
+        ></div>
+
+        <div className="fixed items-center p-5 z-50 h-80 md:h-68 w-3/5 lg:w-2/5  bg-purple-600 rounded-lg">
+          <div className="border-2 rounded border-white h-full w-full flex flex-col justify-center items-center space-y-6">
+            <div
+              onClick={closeModal}
+              className="w-full text-white px-5 flex justify-end font-bold text-3xl cursor-pointer"
+            >
+              x
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-sm sm:text-sm md:text-2xl text-white font-bold m-0">
+                {props.cardData.unity}
+              </h2>
+              <h3 className="m-0">Level:{props.cardData.level}</h3>
+            </div>
+            <div className="flex flex-col h-full w-full md:flex-row justify-center items-center md:space-x-5 space-y-2 md:space-y-0 ">
+              <Button secondary sm>
+                I Agree
+              </Button>
+              <Button onClick={closeModal} danger sm>
+                Return
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
